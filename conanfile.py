95a6dd368f09e5d5c27bf86a123f0a933192751e
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-08-08 20:01:59 +0300

    Boost stable version

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class WebsocketppConan(ConanFile):
     no_copy_source = True
 
     def requirements(self):
-        self.requires("boost/[>=1.54.0]@%s/testing" % self.user)
+        self.requires("boost/[>=1.54.0]@%s/stable" % self.user)
 
     def source(self):
         tools.patch(patch_file="odant.patch")
